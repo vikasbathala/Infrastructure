ea37f7fd0207f2b4ba897782e80dabe24d51ea25
vikasbathala <vikasbathala@gmail.com>
2026-03-11 15:46:06 +0530

    Update backendstg.tf

--- backendstg.tf
+++ backendstg.tf
@@ -1,9 +1,9 @@
 # #migrate state file to storage account - block
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG_Name_Here"
-    storage_account_name = "Storage_Account_Name_Here"
-    container_name       = "Container_Name_Here"
-    key                  = "Key_Name_Here"
+    resource_group_name  = "vikassa_rg"
+    storage_account_name = "vikasinfrastgacc"
+    container_name       = "vikasinfra"
+    key                  = "tfstate"
   }
 }
