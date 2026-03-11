395df7ebc875b09c5bf9c383842b83bf66080b61
vikasbathala <vikasbathala@gmail.com>
2026-03-11 15:46:44 +0530

    Replace placeholder IDs with actual Azure credentials
    Updated Azure provider configuration with actual subscription and tenant IDs.

--- provider.tf
+++ provider.tf
@@ -8,8 +8,8 @@ terraform {
   #required_version = ">= 1.7.0"
 }
 provider "azurerm" {
-  subscription_id = "ADD_SUBSCRIPTION_ID_HERE"
-  tenant_id       = "ADD_TENANT_ID_HERE"
+  subscription_id = "d7d608df-45e4-4c0b-b403-52488bb99d55"
+  tenant_id       = "0c90027b-7c4e-4870-be8a-38b2bf6cf020"
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
